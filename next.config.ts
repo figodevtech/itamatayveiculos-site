42220308033cffafa0f93bb4a598ac7daba244b3
FIGO <figo.devtech@gmail.com>
2026-04-03 00:30:58 -0300

    disable image optimization

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     minimumCacheTTL: 2678400,
     formats: ["image/webp"],
     remotePatterns: [
